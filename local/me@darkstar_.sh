5ef53e7631c19c5b99e863c671b8934c3037884b
wd5gnr <al.williams@awce.com>
2017-04-04 23:28:23 -0500

    Fixed host name in comments

--- local/me@darkstar_.sh
+++ local/me@darkstar_.sh
@@ -1,4 +1,4 @@
-# This script will run only for host enterprise
+# This script will run only for host darkstar
 # and will run before all other scripts
 # Normally you are better off doing things in @enterprise so
 # all other set up is ready
